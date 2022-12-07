f5321c470e41d702272c40077b0c273b06b1a5a5
Cameron Fuller <77114777+ctfuller98@users.noreply.github.com>
2022-12-06 21:29:27 -0700

    scaffolded member

---
+++ SacramentMeetingPlanner/Pages/Member/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SacramentMeetingPlanner.Data;
+using SacramentMeetingPlanner.Models;
+
+namespace SacramentMeetingPlanner.Pages.Member
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SacramentMeetingPlanner.Data.SacramentMeetingPlannerContext _context;
+
+        public DetailsModel(SacramentMeetingPlanner.Data.SacramentMeetingPlannerContext context)
+        {
+            _context = context;
+        }
+
+      public Member Member { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Member == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Member.FirstOrDefaultAsync(m => m.ID == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Member = member;
+            }
+            return Page();
+        }
+    }
+}
