1ed9d4919f507db9e9ce7895d99c4ee3281f638b
DAREN TIBBITTS <tomtomm@byui.edu>
2022-12-06 20:49:14 -0700

    Member model work

--- SacramentMeetingPlanner/Models/Member.cs
+++ SacramentMeetingPlanner/Models/Member.cs
@@ -7,6 +7,31 @@ namespace SacramentMeetingPlanner.Models
     public class Member
     {
         public int ID { get; set; }
+
+        [Display (Name = "First Name")]
+        [StringLength(20, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(20, MinimumLength = 3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display (Name = "Title")]
+        [Required]
+        public Title MemberTitle { get; set; }
+    }
+
+    public enum Title
+    {
+        President,
+        Bishop,
+        Elder,
+        Sister,
+        Brother,
+        Patriarch
     }
 }
