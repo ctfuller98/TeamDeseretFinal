6b0606c598ab0feb7fea9200a70db5c20bb9b0ac
DAREN TIBBITTS <tomtomm@byui.edu>
2022-12-06 20:30:50 -0700

    Added Members Model.

---
+++ SacramentMeetingPlanner/Models/Member.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Member
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+    }
+}
