8f4b213964656d0e71904a7ac8b70c21812ba081
DAREN TIBBITTS <tomtomm@byui.edu>
2022-12-07 20:42:06 -0700

    Member Seed data.

---
+++ SacramentMeetingPlanner/Models/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SacramentMeetingPlanner.Data;
+using System;
+using System.Linq;
+
+namespace SacramentMeetingPlanner.Models
+
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SacramentMeetingPlannerContext(
+                       serviceProvider.GetRequiredService<
+                           DbContextOptions<SacramentMeetingPlannerContext>>()))
+            {
+                // Look for any members.
+                if (context.Member.Any())
+                {
+                    // DB has been seeded. Do nothing.
+                    return;
+                }
+
+                context.Member.AddRange(
+                    new Member
+                    {
+                        FirstName = "Cameron",
+                        LastName = "Fuller",
+                        MemberTitle = Title.Bishop
+                    },
+
+                    new Member
+                    {
+                        FirstName = "Austin",
+                        LastName = "Donovan",
+                        MemberTitle = Title.President
+                    },
+
+                    new Member
+                    {
+
+                    }
+
+                );
+            }
+        }
+    }
+}
